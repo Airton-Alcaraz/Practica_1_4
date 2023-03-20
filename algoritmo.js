1fca7207a52c261285343d4f1a82d461ccc8bdff
airton <airton.alcaraz94@gmail.com>
2023-03-20 20:01:45 -0300

    Importante

---
+++ algoritmo.js
@@ -0,0 +1,10 @@
+document.querySelector("body").innerHTML = `
+<details>
+    <summary>Importante</summary>
+    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Incidunt rem possimus sunt temporibus, nostrum asperiores
+    rerum vero maxime nulla maiores vel recusandae, adipisci inventore fuga provident animi quisquam explicabo! Id?
+<img height="300" width="300" src="https://www.fundeu.es/wp-content/uploads/2021/05/boxeo-efespthirteen411921.jpg" alt="">
+<iframe src="https://www.mercadolibre.com.ar/" frameborder="0"></iframe> 
+<br>
+<a href="https://google.com">LINK</a>
+`;
